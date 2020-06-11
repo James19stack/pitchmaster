d70c961cfd1cf99834cfabe146d1a0cefaa79e14
James Mirara <mirarajames29@gmail.com>
2020-06-12 00:40:13 +0300

    Add changes made

--- app/models.py
+++ app/models.py
@@ -1,5 +1,14 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 class Pitch:
-    def __init__(self,id,pitch,pitch_category,posted,upvotes,downvotes,comment,)
+    def __init__(self,id,pitch,pitch_category,posted,upvotes,downvotes,comment):
         self.id = id
         self.pitch = pitch
         self.pitch_category = pitch_category
@@ -9,8 +18,42 @@ class Pitch:
         self.comment = comment
 
 class Comment:
-    def __init__(self,id,comment,posted)  
+    def __init__(self,id,comment,posted):  
         self.id = id
         self.comment = comment
         self.posted = posted      
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_hash = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
     
+    def __repr__(self):
+        return f'User {self.username}'    
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+    def __repr__(self):
+        return f'User {self.name}'
